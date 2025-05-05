adb8029b565d5abb422740b4d0444eeec6cddfb2
Purohitdev <devp61285@gmail.com>
2025-05-05 19:21:05 +0530

    2s

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); 
+    }, 2000); 
 
     return () => clearTimeout(timer);
   }, []);
